adf826236ff23baf544064499717b2a06eae8204
Franco Mastrantonio <fmastrantonio.knh@veritran.com>
2025-03-28 15:21:27 -0300

    se agregan icons de stack

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -19,9 +19,11 @@ export default function ProjectsSection(){
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <div className="flex flex-col">
-                        <span className="text-xl font-semibold flex">{project.title}</span>
-                        <p className="text-parrafo pl-0 mb-2 flex">{project.description}</p>
+                    <div className="flex flex-col justify-between h-[100%]">
+                        <div>
+                            <span className="text-xl font-semibold flex">{project.title}</span>
+                            <p className="text-parrafo pl-0 mb-2 flex">{project.description}</p>
+                        </div>
                         <Image 
                             className="mb-2 flex"
                             src={`/images${project.preview}`}
@@ -30,6 +32,25 @@ export default function ProjectsSection(){
                             height={779}
                             priority
                         />
+                        <div className="flex flex-row justify-around flex-wrap mt-1">
+                            {
+                                project.stack.map((tech)=>{
+                                    return (
+                                        <div className="flex flex-row w-fit items-center mr-2 mb-1" key={tech.id}>
+                                            <Image 
+                                                className="flex mr-2"
+                                                src={`/icons/${tech.id}-icon.svg`}
+                                                alt={`icono de ${tech.id}`}
+                                                width={20}
+                                                height={20}
+                                                priority
+                                            />
+                                            <span className="flex w-auto">{tech.description}</span>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </a>
                 ) )
